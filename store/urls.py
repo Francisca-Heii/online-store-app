bf71cffa80ad88bde386f6ab981c886fe568a9b3
Francisca-Heii <franciscahheii@gmail.com>
2022-01-17 19:30:54 +0000

    Added store app and templates (corrections)

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from .import views
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='store')
+    path('', views.index,name='store'),
     
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def index (request):
-    """ A view to retuen the index page """
-    return render(request, 'store/index.html')
+def index(request):
+    return render (request,'store/index.html')
