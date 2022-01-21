974b6802cc9b3c08915219d43e6531eb01be041e
Francisca-Heii <franciscahheii@gmail.com>
2022-01-21 16:32:14 +0000

    Added products views and templates

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('store/', include('store.urls')),      
+    path('store/', include('store.urls')),
+     path('products/', include('products.urls'))      
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
+def all_products(request):
+    products =product.objects.all()
+    context = {
+        'products': products,
+        
+    }
+
+    return render (request,'products/products.html')
+
+
