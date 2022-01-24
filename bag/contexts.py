29e594afc8fdbc7481b8e3ecb4bdafeeadd732be
Francisca-Heii <franciscahheii@gmail.com>
2022-01-24 00:36:14 +0000

    Added add to cart functionality and +/- quantity buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,9 +1,38 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import product
+
+
 def bag_contents(request):
+
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            Product = get_object_or_404(product, pk=item_id)
+            total += item_data * Product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': Product,
+            })
+        else:
+            Product = get_object_or_404(product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': Product,
+                    'size': size,
+                })
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
@@ -24,5 +53,3 @@ def bag_contents(request):
     }
 
     return context
-
-
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+
 
 # Create your views here.
 def views_bag(request):
     return render (request,'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    request.session['bag'] = bag
+    return redirect(redirect_url)         
