30e3b8ac0e3824fc871d0d78b3ea0754a83b784b
Francisca-Heii <franciscahheii@gmail.com>
2022-01-26 20:32:49 +0000

    Updated bag, checkout apps and added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
+
+
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -10,30 +14,18 @@ from products.models import product
 from bag.contexts import bag_contents
 
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
          messages.error(request, "There's nothing in your bag at the moment")
          return redirect(reverse('products'))
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-        )
-    print(intent)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
-
-    template = 'checkout/checkout.html'
+    
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KLyKFJs0stgofJHHPjoPZQdthBt6CnGr34zFmmu1lxXC5ppTv9FF1zgKUfXWicA4uY9KmQiM3fyvGsybuiQaS3W00n3B3PFYH',
+        'client_secret': 'test client secret',
     }
 
 
