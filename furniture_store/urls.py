7e2208dfd9526f5f45e2b3db6a06b3c3c1d2f994
Francisca-Heii <franciscahheii@gmail.com>
2022-01-28 19:58:27 +0000

    Added hover effect to navbar, slider images and css

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -22,10 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('store/', include('store.urls')),
-    path('products/', include('products.urls')), 
+    path('', include('products.urls')), 
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
-
-         
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('profile/', include('profiles.urls')),         
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
