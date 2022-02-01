21ecd8d7f46d2a271901acb465e2de487d6bd67e
Francisca-Heii <franciscahheii@gmail.com>
2022-02-01 08:52:09 +0000

    Modified the main app url and product_details.html

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('store/', include('store.urls')),
-    path('', include('products.urls')), 
+    path('products/', include('products.urls')), 
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),    
