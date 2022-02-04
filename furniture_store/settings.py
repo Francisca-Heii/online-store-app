9be20f25dd6459f3bc0208f45e4f278b598cc4e1
Francisca-Heii <franciscahheii@gmail.com>
2022-02-04 01:14:18 +0000

    Defined AWS_ACCESS_KEY_ID

--- furniture_store/settings.py
+++ furniture_store/settings.py
@@ -177,7 +177,7 @@ if 'USE_AWS' in os.environ:
     #Bucket config
     AWS_STORAGE_BUCKET_NAME='supremefurnitures'
     AWS_S3_REGION_NAME='us-east-1'
-    AWS_ACCESS_KEY=os.environ.get('AWS_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY')
     AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN=f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
