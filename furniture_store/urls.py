289409bb947e08f20615a0dd4499d35d51c4ebc3
Francisca-Heii <franciscahheii@gmail.com>
2022-02-04 18:48:33 +0000

    Urls updated

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('store/', include('store.urls')),
-    path('', include('products.urls')), 
+    path('', include('store.urls')),
+    path('products/', include('products.urls')), 
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),    
--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('new', views.index,name='store'),
+    path('', views.index,name='store'),
     
 ]
