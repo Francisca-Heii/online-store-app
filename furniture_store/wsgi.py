c542a1e1b2be2df144bbacf698d38c1270166bd3
touseefurehman <tousifurehman2084@gmail.com>
2025-11-26 07:31:53 +0500

    static issue solved

--- furniture_store/wsgi.py
+++ furniture_store/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furniture_store.settings')
 
 application = get_wsgi_application()
+
+# Add this for WhiteNoise
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
