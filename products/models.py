d91390829745061a16dc55c883048908809d5898
touseefurehman <tousifurehman2084@gmail.com>
2026-01-04 11:39:43 +0500

    cat issue solve

--- products/models.py
+++ products/models.py
@@ -3,17 +3,28 @@
 from unicodedata import category
 from unittest import skipUnless
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
 class Category(models.Model):
     class Meta():
         verbose_name_plural='Categories'
-    name= models.CharField(max_length=254)
-    friendly_name=models.CharField(max_length=254,null=True,blank=True)
+    
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    slug = models.SlugField(max_length=254, unique=True, null=True, blank=True)  # Add this
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return self.name
+    
     def get_friendly_name(self):
         return self.friendly_name
+    
+
 class product(models.Model):
     category=models.ForeignKey('Category',null=True,blank=True,on_delete=models.SET_NULL)
     sku=models.CharField(max_length=254,null=True,blank=True)
--- products/views.py
+++ products/views.py
@@ -5,13 +5,73 @@ from .models import product,Category
 from django.db.models.functions import Lower
 
 
-# Create your views here.
+# # Create your views here.
+# def all_products(request):
+#     products = product.objects.all()
+#     query = None
+#     categories=None
+#     sort=None
+#     direction=None
+    
+#     if request.GET:
+#         if 'sort' in request.GET:
+#             sortkey = request.GET['sort']
+#             sort = sortkey
+#             if sortkey == 'name':
+#                 sortkey = 'lower_name'
+#                 products = products.annotate(lower_name=Lower('name'))
+#             if sortkey == 'category':
+#                 sortkey = 'category__name'
+#             if 'direction' in request.GET:
+#                 direction = request.GET['direction']
+#                 if direction == 'desc':
+#                     sortkey = f'-{sortkey}'
+#             products = products.order_by(sortkey)
+
+#         if 'category' in request.GET:
+#             categories = request.GET['category'].split(',')
+#             print("Total received",categories)
+#             products = products.filter(category__name__in=categories)
+#             categories =Category.objects.filter(name__in=categories)
+
+        
+#         if 'q' in request.GET:
+#             category=['Sofa', 'TV Unit', 'Coffee Table', 'Beds', 'Matressess', 'Wardrobes', 'Dinning Tables', 'Dinning Chair', 'Dinning Set', 'Desk', 'Office Chair', 'Candles and Holders', 'Throws and Blankets', 'Table Lamp', 'Ornamental items', 'Ceiling Lamp', 'Floor Lamp']
+#             category={"Sofa":"Sofa", "TV unit":"tv_unit", }
+#             query = request.GET['q']
+#             print("query ",query)
+#             if not query:
+#                 messages.error(request, "You didn't enter any search criteria!")
+#                 return redirect(reverse('products'))
+#             queries = Q(name__icontains=query) | Q(description__icontains=query)
+#             products = products.filter(queries)
+#             print("jajdksk",queries)
+#     current_sorting = f'{sort}_{direction}'
+            
+
+#     context = {
+#         'products': products,
+#         'search_term': query,
+#         'current_categories': categories,
+#         'current_sorting': current_sorting,
+#     }
+
+#     return render (request,'products/products.html',context)
+
+
+
+
+
+
+
+
+
 def all_products(request):
     products = product.objects.all()
     query = None
-    categories=None
-    sort=None
-    direction=None
+    categories = None
+    sort = None
+    direction = None
     
     if request.GET:
         if 'sort' in request.GET:
@@ -29,26 +89,122 @@ def all_products(request):
             products = products.order_by(sortkey)
 
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            print("Total received",categories)
-            products = products.filter(category__name__in=categories)
-            categories =Category.objects.filter(name__in=categories)
+            url_categories = request.GET['category'].split(',')
+            print("DEBUG - URL categories:", url_categories)
+            
+            # Map URL slugs to actual category names
+            slug_to_name = {
+                'sofa': 'Sofa',
+                'tv_unit': 'TV Unit',  # This was correct
+                'coffee_table': 'Coffee Table',
+                'beds': 'Beds',
+                'mattresses': 'Mattresses',
+                'matressess': 'Mattresses',
+                'wardrobes': 'Wardrobes',
+                'dining_tables': 'Dining Tables',
+                'dinning_tables': 'Dining Tables',
+                'dining_chair': 'Dining Chair',
+                'dinning_chair': 'Dining Chair',
+                'dining_set': 'Dining Set',
+                'dinning_set': 'Dining Set',
+                'desk': 'Desk',
+                'office_chair': 'Office Chair',
+                'candles_and_holders': 'Candles and Holders',
+                'throws_and_blankets': 'Throws and Blankets',
+                'table_lamp': 'Table Lamp',
+                'ornamental_items': 'Ornamental Items',
+                'ceiling_lamp': 'Ceiling Lamp',
+                'floor_lamp': 'Floor Lamp'
+            }
+            
+            # Convert URL categories to actual names
+            category_names = []
+            for cat in url_categories:
+                # Check if it's in our mapping
+                if cat.lower() in [k.lower() for k in slug_to_name.keys()]:
+                    # Find the correct key (case-insensitive)
+                    key = next(k for k in slug_to_name.keys() if k.lower() == cat.lower())
+                    category_names.append(slug_to_name[key])
+                else:
+                    # Handle special cases
+                    guessed_name = cat.replace('_', ' ').replace('-', ' ').title()
+                    # Fix common acronyms
+                    if 'tv' in cat.lower():
+                        guessed_name = guessed_name.replace('Tv', 'TV')
+                    category_names.append(guessed_name)
+            
+            print("DEBUG - Looking for categories:", category_names)
+            
+            # Filter products
+            products = products.filter(category__name__in=category_names)
+            
+            # Get the actual Category objects
+            categories = Category.objects.filter(name__in=category_names)
+            
+            print("DEBUG - Found categories:", list(categories.values_list('name', flat=True)))
+            print("DEBUG - Product count:", products.count())
+        # if 'category' in request.GET:
+        #     url_categories = request.GET['category'].split(',')
+        #     print("DEBUG - URL categories:", url_categories)
+            
+        #     # Map URL slugs to actual category names
+        #     slug_to_name = {
+        #         'sofa': 'Sofa',
+        #         'tv_unit': 'TV Unit',
+        #         'coffee_table': 'Coffee Table',
+        #         'beds': 'Beds',
+        #         'mattresses': 'Mattresses',
+        #         'matressess': 'Mattresses',  # Handle typo
+        #         'wardrobes': 'Wardrobes',
+        #         'dining_tables': 'Dining Tables',
+        #         'dinning_tables': 'Dining Tables',  # Handle typo
+        #         'dining_chair': 'Dining Chair',
+        #         'dinning_chair': 'Dining Chair',  # Handle typo
+        #         'dining_set': 'Dining Set',
+        #         'dinning_set': 'Dining Set',  # Handle typo
+        #         'desk': 'Desk',
+        #         'office_chair': 'Office Chair',
+        #         'candles_and_holders': 'Candles and Holders',
+        #         'throws_and_blankets': 'Throws and Blankets',
+        #         'table_lamp': 'Table Lamp',
+        #         'ornamental_items': 'Ornamental Items',
+        #         'ceiling_lamp': 'Ceiling Lamp',
+        #         'floor_lamp': 'Floor Lamp'
+        #     }
+            
+        #     # Convert URL slugs to actual names
+        #     category_names = []
+        #     for slug in url_categories:
+        #         if slug in slug_to_name:
+        #             category_names.append(slug_to_name[slug])
+        #         else:
+        #             # Try to guess the name
+        #             guessed_name = slug.replace('_', ' ').title()
+        #             category_names.append(guessed_name)
+            
+        #     print("DEBUG - Looking for categories:", category_names)
+            
+        #     # Filter products
+        #     products = products.filter(category__name__in=category_names)
+            
+        #     # Get the actual Category objects
+        #     categories = Category.objects.filter(name__in=category_names)
+            
+        #     print("DEBUG - Found categories:", list(categories.values_list('name', flat=True)))
+        #     print("DEBUG - Product count:", products.count())
 
-        
         if 'q' in request.GET:
-            category=['Sofa', 'TV Unit', 'Coffee Table', 'Beds', 'Matressess', 'Wardrobes', 'Dinning Tables', 'Dinning Chair', 'Dinning Set', 'Desk', 'Office Chair', 'Candles and Holders', 'Throws and Blankets', 'Table Lamp', 'Ornamental items', 'Ceiling Lamp', 'Floor Lamp']
-            category={"Sofa":"Sofa", "TV unit":"tv_unit", }
             query = request.GET['q']
-            print("query ",query)
+            print("query ", query)
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            print("jajdksk",queries)
+            print("search results:", products.count())
+    
     current_sorting = f'{sort}_{direction}'
-            
-
+    
     context = {
         'products': products,
         'search_term': query,
@@ -56,7 +212,11 @@ def all_products(request):
         'current_sorting': current_sorting,
     }
 
-    return render (request,'products/products.html',context)
+    return render(request, 'products/products.html', context)
+
+
+
+
 
 def product_detail(request, product_id):
     print("product_id", product_id)
